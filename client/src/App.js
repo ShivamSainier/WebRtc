7eba2241d5ecfcb1275fd1f1bea212f022e88ce2
shivam saini <shivamsainier98@gmail.com>
2022-10-30 12:17:24 +0530

    add client

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+function App() {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+         Video Chat
+        </Toolbar>
+      </AppBar>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/compents/VideoPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VideoPlayer() {
+  return (
+    <div>
+      Video Player
+    </div>
+  )
+}
+
+export default VideoPlayer
